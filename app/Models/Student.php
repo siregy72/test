a41c7b72a7960cd5d001ef2043f1c9d7fd690047
chokj <siregy72@gmail.com>
2025-08-31 23:52:38 +0900

    시딩딩

---
+++ app/Models/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'class_id', 'section_id'];
+
+    public function class(): BelongsTo
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+
+    public function section(): BelongsTo
+    {
+        return $this->belongsTo(Section::class);
+    }
+}
